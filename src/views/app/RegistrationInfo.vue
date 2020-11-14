81077572c05f454f6f16de9e6d35cbb09e75f92f
daniellygj <daniellygj@gmail.com>
2020-11-14 20:18:03 -0300

    voltando com a registrationInfo

--- src/views/app/RegistrationInfo.vue
+++ src/views/app/RegistrationInfo.vue
@@ -1,21 +1,26 @@
 <template>
-  <academic-info />
+  <div class="registration-info-view">
+    <el-tabs value="info" tab-position="left">
+      <el-tab-pane label="Informações pessoais" name="info">
+        <PersonalInfo
+          v-if="page_title == 'Informações pessoais'"
+          :user="user"
+        />
+      </el-tab-pane>
+      <el-tab-pane label="Informações profissionais" name="job">Informações profissionais</el-tab-pane>
+      <el-tab-pane label="Informações acadêmicas" name="school">Informações acadêmicas</el-tab-pane>
+      <el-tab-pane label="Medalhas" name="medals">Medalhas</el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script>
-import AcademicInfo from '@/components/UserInfo/AcademicInfo.vue'
-import UserService from '@/services/UserService.js'
+import PersonalInfo from '@/components/UserInfo/PersonalInfo.vue'
 
 export default {
   name: 'RegistrationInfo',
   components: {
-    AcademicInfo
-  },
-  mounted () {
-    UserService.getUser()
-      .then((res) => {
-        this.user = res
-      })
+    PersonalInfo
   }
 }
 </script>
